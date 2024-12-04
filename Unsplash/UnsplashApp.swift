1c5d32019675cb77e0cc364eb12a385219f1b1e2
MAZEL-Florian <mazel.florian1@gmail.com>
2024-12-04 14:27:36 +0100

    Initial Commit

---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by MAZEL Florian on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
